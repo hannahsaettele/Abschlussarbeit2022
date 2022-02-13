5f74b3c2832d04ed62ee86098b32689494ccfe51
Hannah Sättele <hannah.saettele@hs-furtwangen.de>
2022-02-13 13:55:03 +0100

    Einstellungen döner trainer

--- Script/main.js
+++ Script/main.js
@@ -2,12 +2,14 @@
 var Dönerladen;
 (function (Dönerladen) {
     /*Abschlussarbeit, Hannah Sättele, Matrikelnr. 268473, erstellt.: 27.01.22;
-    In Zusammenarbeit mit: Lukas D.
+    Inspiration: Lukas D.
     */
     window.addEventListener("load", handleLoad);
     let background;
     //Eingabefelder Quelle: Sequenzmemory
     function handleLoad(_event) {
+        let startButton = document.querySelector("#startButton"); // mit klick auf Start, wird Döner-Trainer erstellt
+        startButton.addEventListener("click", startTrainer);
         let canvas = document.querySelector("canvas");
         if (!canvas)
             return;
@@ -15,33 +17,22 @@ var Dönerladen;
         //alle div Elemente klickbar machen
         let divs = document.querySelectorAll("div");
         for (let i = 0; i > divs.length; i++) {
-            divs[i].addEventListener("click", doIt);
+            divs[i].addEventListener("click", startTrainer);
         }
         startTrainer();
-        createContainer();
-    }
-    function doIt() {
-        //
     }
     function startTrainer() {
         drawBackground();
-        background = Dönerladen.crc2.getImageData(0, 0, Dönerladen.crc2.canvas.width, Dönerladen.crc2.canvas.height);
+        background = Dönerladen.crc2.getImageData(0, 0, Dönerladen.crc2.canvas.width, Dönerladen.crc2.canvas.height); // Boden
+        Dönerladen.crc2.fillStyle = "white";
+        Dönerladen.crc2.fillRect(0, 0, (Dönerladen.crc2.canvas.width * 0.4), Dönerladen.crc2.canvas.height);
+        Dönerladen.crc2.fillRect(0, 0, Dönerladen.crc2.canvas.width * 0.6, Dönerladen.crc2.canvas.height * 0.15);
+        Dönerladen.crc2.fillRect(Dönerladen.crc2.canvas.width * 0.6, 0, Dönerladen.crc2.canvas.width * 0.1, Dönerladen.crc2.canvas.height);
+        Dönerladen.crc2.fillRect(0, Dönerladen.crc2.canvas.height * 0.85, Dönerladen.crc2.canvas.width * 0.6, Dönerladen.crc2.canvas.height * 0.15);
     }
     function drawBackground() {
-        Dönerladen.crc2.fillStyle = "#FFC0CB";
+        Dönerladen.crc2.fillStyle = "#FF69B4";
         Dönerladen.crc2.fillRect(0, 0, Dönerladen.crc2.canvas.width, Dönerladen.crc2.canvas.height);
-    }
-    function createContainer(_position) {
-        Dönerladen.crc2.save();
-        Dönerladen.crc2.beginPath();
-        // Stamm erstellen
-        Dönerladen.crc2.translate(500, 500);
-        Dönerladen.crc2.moveTo(0, 0);
-        Dönerladen.crc2.rect(-5, -20, 10, 20); // beginn links oben rectangle Rechteck
-        Dönerladen.crc2.closePath();
-        Dönerladen.crc2.fillStyle = "darkrosa";
-        Dönerladen.crc2.fill();
-        let container = new Path2D();
     }
 })(Dönerladen || (Dönerladen = {}));
 //# sourceMappingURL=main.js.map
--- Script/main.ts
+++ Script/main.ts
@@ -1,6 +1,6 @@
 namespace Dönerladen {
     /*Abschlussarbeit, Hannah Sättele, Matrikelnr. 268473, erstellt.: 27.01.22; 
-    In Zusammenarbeit mit: Lukas D. 
+    Inspiration: Lukas D. 
     */
 
     window.addEventListener("load", handleLoad);
@@ -9,59 +9,43 @@ namespace Dönerladen {
     let background: ImageData;
 
     //Eingabefelder Quelle: Sequenzmemory
-  
+
 
     function handleLoad(_event: Event): void {
+
+        let startButton: HTMLButtonElement = <HTMLButtonElement>document.querySelector("#startButton"); // mit klick auf Start, wird Döner-Trainer erstellt
+        startButton.addEventListener("click", startTrainer);
 
         let canvas: HTMLCanvasElement | null = document.querySelector("canvas");
         if (!canvas)
             return;
-        crc2 = <CanvasRenderingContext2D>canvas.getContext("2d")
+        crc2 = <CanvasRenderingContext2D>canvas.getContext("2d");
 
 
         //alle div Elemente klickbar machen
         let divs: NodeListOf<HTMLDivElement> = document.querySelectorAll("div");
 
         for (let i: number = 0; i > divs.length; i++) {
-            divs[i].addEventListener("click", doIt);
+            divs[i].addEventListener("click", startTrainer);
         }
 
         startTrainer();
-        createContainer();
     }
-
-    function doIt(): void {
-        //
-    }
-
 
     function startTrainer(): void {
 
-
         drawBackground();
-        background = crc2.getImageData(0, 0, crc2.canvas.width, crc2.canvas.height);
+        background = crc2.getImageData(0, 0, crc2.canvas.width, crc2.canvas.height); // Boden
 
+        crc2.fillStyle = "white";
+        crc2.fillRect(0, 0, (crc2.canvas.width * 0.4), crc2.canvas.height);
+        crc2.fillRect(0, 0, crc2.canvas.width * 0.6, crc2.canvas.height * 0.15);
+        crc2.fillRect(crc2.canvas.width * 0.6, 0, crc2.canvas.width * 0.1, crc2.canvas.height);
+        crc2.fillRect(0, crc2.canvas.height * 0.85, crc2.canvas.width * 0.6, crc2.canvas.height * 0.15);
     }
+
     function drawBackground(): void {
-        crc2.fillStyle = "#FFC0CB";
+        crc2.fillStyle = "#FF69B4";
         crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
-
     }
-
-    function createContainer(_position): void {
-
-        crc2.save();
-        crc2.beginPath();
-            // Stamm erstellen
-        crc2.translate(500, 500);
-        crc2.moveTo(0, 0);
-        crc2.rect( -5 , -20, 10, 20);  // beginn links oben rectangle Rechteck
-        crc2.closePath();
-        crc2.fillStyle = "darkrosa";
-        crc2.fill();
-    
-        let container: Path2D = new Path2D();
-    }
-
-
 }
